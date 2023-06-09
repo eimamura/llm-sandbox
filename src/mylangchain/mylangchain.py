d82d3c82e877246df4cf8fabc835869bb791e7ce
Erielcio Imamura <erielcio00@gmail.com>
2023-06-09 18:46:51 -0300

    add chains

--- src/mylangchain/mylangchain.py
+++ src/mylangchain/mylangchain.py
@@ -1,11 +1,17 @@
 import os
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 
 def main():
     OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
     llm = OpenAI(openai_api_key=OPENAI_API_KEY)
-    # print('Hello World!')
 
-    text = "What would be a good company name for a company that makes colorful socks?"
-    print(llm(text))
+    prompt = PromptTemplate(
+        input_variables=["product"],
+        template="What is a good name for a company that makes {product}?",
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+    chain.run("colorful socks")
