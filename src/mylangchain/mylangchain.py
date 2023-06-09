d8b65b3ba8deef3a2cd45a7942a7e884de3a5c74
Erielcio Imamura <erielcio00@gmail.com>
2023-06-09 18:18:13 -0300

    add settings in vscode config

--- src/mylangchain/mylangchain.py
+++ src/mylangchain/mylangchain.py
@@ -1,7 +1,11 @@
 import os
 from langchain.llms import OpenAI
 
+
 def main():
     OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
     llm = OpenAI(openai_api_key=OPENAI_API_KEY)
     # print('Hello World!')
+
+    text = "What would be a good company name for a company that makes colorful socks?"
+    print(llm(text))
